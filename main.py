494d814fe44cab98e61d534ab826692637f38af8
mijinB <bmj13465@gmail.com>
2024-06-02 22:16:51 +0900

    web site 조작 & get content

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from playwright.sync_api import sync_playwright
+import time
+from bs4 import BeautifulSoup
 
 p = sync_playwright().start()
 
@@ -6,6 +8,31 @@ browser = p.chromium.launch(headless=False)
 
 page = browser.new_page()
 
-page.goto("https://google.com")
+page.goto("https://www.wanted.co.kr/")
 
-page.screenshot(path="screenshot.png")
+time.sleep(5)
+
+page.click("button.Aside_searchButton__Xhqq3")
+
+time.sleep(5)
+
+page.get_by_placeholder("검색어를 입력해 주세요.").fill("flutter")
+
+time.sleep(5)
+
+page.keyboard.down("Enter")
+
+time.sleep(5)
+
+page.click("a#search_tab_position")
+
+for x in range(4):
+  time.sleep(5)
+  
+  page.keyboard.down("End")
+
+content = page.content()
+
+p.stop()
+
+soup = BeautifulSoup(content, "html.parser")
